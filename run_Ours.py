741b09da8c8e07fefbcf7ecdcfd3a995fdcd3820
wissem18 <yousfiwissem224@gmail.com>
2025-07-11 17:06:40 +0200

    change data file name to distinguish between GAT and GATv2

--- run_Ours.py
+++ run_Ours.py
@@ -56,7 +56,7 @@ if __name__ == '__main__':
         else:
             args.block_num = None
 
-        args.model_name = 'Ours_GAT'
+        args.model_name = 'Ours_GATv2'
         args.task = args.task + '_' + args.level
     
 
