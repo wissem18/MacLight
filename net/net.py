9ddd5a25d5153da20e18067a6abce3a812ae4c0a
wiss <yousfiwissem224@gmail.com>
2025-07-15 10:51:37 +0200

    change to GATConv

--- net/net.py
+++ net/net.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch_geometric.nn import GATv2Conv as GATConv
+from torch_geometric.nn import GATConv
 
 
 class PolicyNet(torch.nn.Module):
